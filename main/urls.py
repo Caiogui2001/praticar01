7b5c94636a5eb6950356b6535b2c5c102c130bae
Caiogui2001 <caio2001ti@gmail.com>
2024-11-26 20:44:48 -0300

    ATUALIZAÇÃO

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cadastro_list, name='cadastro_list'),
     path('cadastro/nova/', views.criar_cadastro, name='criar_cadastro'),
+    path('editar/<int:id>/', views.editar_cadastro, name='editar_cadastro'),
+    path('deletar/<int:id>/', views.deletar_cadastro, name='deletar_cadastro'),
 ]
--- main/views.py
+++ main/views.py
@@ -17,3 +17,26 @@ def criar_cadastro(request):
         Cadastro.objects.create(nome=nome, cpf=cpf, data_nascimento=data_nascimento)
         return redirect('cadastro_list')
     return render(request, 'cadastro/criar_cadastro.html')
+
+def editar_cadastro(request, id):
+    cadastro = get_object_or_404(Cadastro, id=id)  # Busca o cadastro pelo id
+    
+    if request.method == 'POST':
+        # Atualiza os campos do cadastro com os dados do formulário
+        cadastro.nome = request.POST['nome']
+        cadastro.cpf = request.POST['cpf']
+        cadastro.data_nascimento = request.POST['data_nascimento']
+        cadastro.save()  # Salva as alterações
+        
+        return redirect('cadastro_list')  # Redireciona para a lista de cadastros
+    
+    return render(request, 'editar_cadastro.html', {'cadastro': cadastro})
+
+def deletar_cadastro(request, id):
+    cadastro = get_object_or_404(Cadastro, id=id)  # Busca o cadastro pelo id
+    
+    if request.method == 'POST':
+        cadastro.delete()  # Exclui o cadastro
+        return redirect('cadastro_list')  # Redireciona para a lista de cadastros
+    
+    return render(request, 'deletar_cadastro.html', {'cadastro': cadastro})
